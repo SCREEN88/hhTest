e939daf4ecb2c80ffcbbaefb3b8d5803440a2db0
ymarinov <ymarinov@agent.ru>
2015-07-14 19:23:00 +0300

    fix initial page load

--- src/main/java/hh/price/selenium/ServiceBuying.java
+++ src/main/java/hh/price/selenium/ServiceBuying.java
@@ -1,5 +1,6 @@
 package hh.price.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.LoadableComponent;
 
@@ -13,13 +14,21 @@ public class ServiceBuying extends LoadableComponent<ServiceBuying> {
     @Override
     protected void load() {
         driver.get(DefaultProperties.DATA.getString("selenium.testURL"));
+        driver.findElement(By.className("link-secondary")).click();
+        new WaitFor(driver).pageTitleInHeader("Выбор города");
+        driver.findElement(By.cssSelector(".area-switcher-welcome .area-switcher-welcome__item:nth-child(1) li:nth-child(1) > a")).click();
     }
 
     @Override
     protected void isLoaded() throws Error {
-        if (!driver.getCurrentUrl().equals(DefaultProperties.DATA.getString("selenium.testURL"))){
+        new WaitFor(driver).pageTitleInHeader("Покупка услуг");
+        if (wrongStartUrl()){
             throw new Error();
         }
+    }
+
+    private boolean wrongStartUrl() {
+        return !driver.getCurrentUrl().contains(DefaultProperties.DATA.getString("selenium.testURL"));
     }
 
     public WebDriver getDriver() {
