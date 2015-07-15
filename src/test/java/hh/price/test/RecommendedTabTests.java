462abc85d9ca81771fcd43abea08a3fff9f40906
ymarinov <ymarinov@agent.ru>
2015-07-15 15:51:26 +0300

    create correctCartSumTest

--- src/test/java/hh/price/test/RecommendedTabTests.java
+++ src/test/java/hh/price/test/RecommendedTabTests.java
@@ -35,13 +35,32 @@ public class RecommendedTabTests extends DriverInit{
             button.click();
             Map<String, String> cartData = cart.collectCartData().get(iter);
             assert cartData.get("title").contains(offersData.get(iter).get("title"));
-            if (cartData.get("oldPrice").length() > 0){
+            if (cartData.containsKey("oldPrice")){
                 assert cartData.get("oldPrice").contains(offersData.get(iter).get("oldPrice"));
             }
             assert cartData.get("price").contains(offersData.get(iter).get("price"));
             assert cartData.get("bonus").equals(offersData.get(iter).get("bonus").replace("+", ""));
             iter++;
         }
+    }
 
+    @Test
+    public void correctCartSumTest(){
+        RecommendedTab page = new RecommendedTab(getDriver()).get();
+        PriceCart cart = new PriceCart(getDriver().findElement(By.className("HH-PriceCart")));
+        List<Map<String,String>> offersData = page.collectOffersData();
+        long totalSum = 0;
+        long actualSum = 0;
+        int iter= 0;
+        for (WebElement button : page.formButtons) {
+            button.click();
+            if (offersData.get(iter).containsKey("oldPrice")){
+                totalSum += Long.parseLong(offersData.get(iter).get("oldPrice").replace(" руб.", ""));
+            }
+            actualSum += Long.parseLong(offersData.get(iter).get("price").replace(" руб.", ""));
+            assert cart.collectCartData().get(iter).get("totalCost").replace("руб.", "").equals(Long.toString(totalSum));
+            assert cart.collectCartData().get(iter).get("actualCost").replace("руб.", "").equals(Long.toString(actualSum));
+            iter++;
+        }
     }
 }
